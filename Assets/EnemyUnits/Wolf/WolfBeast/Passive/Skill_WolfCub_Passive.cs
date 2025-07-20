836dac0a43fe4daf54d0137ab11ad954261d9670
parkjunesong <parkjunesong@gmail.com>
2025-07-21 00:12:46 +0900

    Unit: WolfBeast Update

---
+++ Assets/EnemyUnits/Wolf/WolfBeast/Passive/Skill_WolfCub_Passive.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+[CreateAssetMenu(fileName = "Skill_WolfBeast_Passive", menuName = "Scriptable Object/SkillData/WolfBeast_Passive", order = int.MaxValue)]
+
+public class Skill_WolfBeast_Passive : Skill_Base
+{   
+    public override void SetEffect()
+    {
+
+    }
+    public override void Execute(Unit caster)
+    {
+    }
+}
